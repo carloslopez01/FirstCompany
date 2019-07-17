1c6e2c6947dd03203b0b1e2948e1ab3f9c10a646
PC1 <PC1@192.168.0.25>
2019-07-17 17:02:30 -0500

    Version 0.001

---
+++ src/java/modelo/EmpleadoDAO.java
@@ -0,0 +1,26 @@
+/*
+ * Copyright (C) 2019 PC1
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package modelo;
+
+/**
+ * @author Carlos Lopez
+ * Version: 0.1 Alpha
+ * Contacto: cl640900@gmail.com || carlos.lopez17@itca.edu.sv
+ */
+public class EmpleadoDAO {
+    
+}
